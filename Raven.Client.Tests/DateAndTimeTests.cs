51ee0f3854b4fd81cac5281bc4c1b1a5dc9c5a9f
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-07-30 12:49:21 +0300

    Add client tests, which will have a reference only to the client projects

---
+++ Raven.Client.Tests/DateAndTimeTests.cs
@@ -0,0 +1,46 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ClientTests.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+using System;
+using Xunit;
+
+namespace Raven.Client.Tests
+{
+	public class DateAndTimeTests : RavenClientTest
+	{
+		public DateAndTimeTests()
+		{
+			using (var session = Store.OpenSession())
+			{
+				for (int i = 0; i < 10; i++)
+				{
+					session.Store(new Post
+					{
+						Id = "posts/" + i,
+						PublishedAt = DateTimeOffset.Now.AddDays(i * -1),
+					});
+				}
+				session.SaveChanges();
+			}
+		}
+
+
+		[Fact]
+		public void CanLoad()
+		{
+			using (var session = Store.OpenSession())
+			{
+				var post = session.Load<Post>("posts/1");
+				Assert.NotNull(post);
+			}
+		}
+
+		private class Post
+		{
+			public string Id { get; set; }
+			public DateTimeOffset PublishedAt { get; set; }
+		}
+	}
+}
---
+++ Raven.Client.Tests/RavenClientTest.cs
@@ -0,0 +1,30 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="RavenClientTest.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+using System;
+using Raven.Client.Document;
+
+namespace Raven.Client.Tests
+{
+	public class RavenClientTest : IDisposable
+	{
+		protected readonly IDocumentStore Store;
+
+		public RavenClientTest(int port = 8081)
+		{
+			Store = new DocumentStore {Url = "http://localhost:" + port}.Initialize();
+		}
+
+		protected IDocumentSession OpenSession()
+		{
+			return Store.OpenSession();
+		}
+
+		public void Dispose()
+		{
+			Store.Dispose();
+		}
+	}
+}
